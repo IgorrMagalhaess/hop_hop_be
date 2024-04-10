f9d3874df888b0069c31559002488a84ac14f9b8
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-04-10 16:33:19 -0400

    fix: column naming in activities table

--- db/migrate/20240410165002_create_activities.rb
+++ db/migrate/20240410165002_create_activities.rb
@@ -6,7 +6,7 @@ class CreateActivities < ActiveRecord::Migration[7.1]
       t.string :description
       t.float :lat
       t.float :lon
-      t.string :type
+      t.string :activity_type
       t.integer :expenses
       t.float :rating
       t.references :daily_itinerary, null: false, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_165002) do
     t.string "description"
     t.float "lat"
     t.float "lon"
-    t.string "type"
+    t.string "activity_type"
     t.integer "expenses"
     t.float "rating"
     t.bigint "daily_itinerary_id", null: false
