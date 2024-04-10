30b1b5b78691913af1726559c077665715bdb046
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-04-10 15:28:56 -0400

    feat: trip model validations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,12 @@ class Trip < ApplicationRecord
    has_many :daily_itineraries
    has_many :activities, through: :daily_itineraries
    has_many :accomodations
+
+   validates :name, presence: true
+   validates :location, presence: true
+   validates :start_date, presence: true
+   validates :end_date, presence: true
+   validates :status, presence: true
+   validates :total_budget, presence: true
+   validates :user_id, presence: true
 end
