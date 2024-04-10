e1f86c5ba79d4c120a4d4a7de1105bcb0fd9bb08
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-04-10 09:29:04 -0400

    fix: naming of serializer

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::TripsController < ApplicationController
    def index
       trips = Trip.all
-      render json: TripsSerializer.new(trips)
+      render json: TripSerializer.new(trips)
    end
 
    def show
       trip = Trip.find(params[:id])
-      render json: TripsSerializer.new(trip)
+      render json: TripSerializer.new(trip)
    end
 end
--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,4 +1,4 @@
-class TripsSerializer
+class TripSerializer
   include JSONAPI::Serializer
   attributes :name, :location, :start_date, :end_date, :status, :total_budget
 end
