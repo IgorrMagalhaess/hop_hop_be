e28eda1d1701120a005bb2b3ad47cd99f161c218
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-04-11 10:30:33 -0400

    refactor: helper method

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::TripsController < ApplicationController
 
    def show
       trip = Trip.find(params[:id])
-      if trip.user_id == params[:user_id].to_i
+      if trip_user_valid?(trip.user_id, params[:user_id])
          render json: TripSerializer.new(trip)
       else
          render_user_error(params)
@@ -15,7 +15,7 @@ class Api::V1::TripsController < ApplicationController
 
    def update
       trip = Trip.find(params[:id])
-      if trip.user_id == params[:user_id].to_i
+      if trip_user_valid?(trip.user_id, params[:user_id])
          trip.update!(trip_params)
          render json: TripSerializer.new(trip)
       else
@@ -32,5 +32,9 @@ class Api::V1::TripsController < ApplicationController
 
    def trip_params
       params.require(:trip).permit(:name, :location, :start_date, :end_date, :status, :total_budget, :user_id)
+   end
+
+   def trip_user_valid?(trip_user_id, user_id)
+      trip_user_id == user_id.to_i
    end
 end
