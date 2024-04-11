0bc6074fe4188ff24194854d2c2bdcf7185f6e02
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-04-11 12:38:09 -0400

    refactor: callback methods for trips controller

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,26 +1,19 @@
 class Api::V1::TripsController < ApplicationController
+   before_action :set_trip, only: [:show, :update]
+   before_action :confirm_user, only: [:show, :update]
+   before_action :filter_user_trips, only: [:index]
+   
    def index
-      trips = Trip.where(user_id: params[:user_id])
-      render json: TripSerializer.new(trips)
+      render json: TripSerializer.new(@trips)
    end
 
    def show
-      trip = Trip.find(params[:id])
-      if trip_user_valid?(trip.user_id, params[:user_id])
-         render json: TripSerializer.new(trip)
-      else
-         render_user_error(params)
-      end
+      render json: TripSerializer.new(@trip)
    end
 
    def update
-      trip = Trip.find(params[:id])
-      if trip_user_valid?(trip.user_id, params[:user_id])
-         trip.update!(trip_params)
-         render json: TripSerializer.new(trip)
-      else
-         render_user_error(params)
-      end
+      @trip.update!(trip_params)
+      render json: TripSerializer.new(@trip)
    end
 
    def create
@@ -34,7 +27,15 @@ class Api::V1::TripsController < ApplicationController
       params.require(:trip).permit(:name, :location, :start_date, :end_date, :status, :total_budget, :user_id)
    end
 
-   def trip_user_valid?(trip_user_id, user_id)
-      trip_user_id == user_id.to_i
+   def set_trip
+      @trip = Trip.find(params[:id])
+   end
+
+   def confirm_user
+      render_user_error(params) if @trip.user_id != params[:user_id].to_i 
+   end
+
+   def filter_user_trips
+      @trips = Trip.where(user_id: params[:user_id])
    end
 end
