055db83444a30a98311f174e82cfdd2a22c52ab1
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-04-11 21:20:20 -0700

    fix: Remove only: [:show] from attributes :daily_itineraries

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -6,7 +6,7 @@ class TripSerializer
     object.activities.sum(&:expenses) || 0
   end
 
-  attribute :daily_itineraries, only: [:show] do |object|
+  attribute :daily_itineraries do |object|
     object.daily_itineraries.each_with_object({}) do |day, daily_itineraries|
       daily_itineraries[day.date] = day.activities
     end
