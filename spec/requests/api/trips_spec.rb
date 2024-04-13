1f468f9c91176eb4cb8f9625932299fea3864f9b
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-04-12 18:41:01 -0700

    test: Add 404 and 400 sad path testing to RSwag

--- spec/requests/api/trips_spec.rb
+++ spec/requests/api/trips_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Trips API', type: :request do
       description "List all Trips that belong to a user with id, location, and name"
       parameter name: :user_id, in: :query, type: :integer
 
-      response(200, 'successful') do
+      response(200, 'Successful') do
         schema type: :object, properties: {
                                 name: {
                                   type: :string,
@@ -23,6 +23,7 @@ RSpec.describe 'Trips API', type: :request do
         let!(:trip1) { create(:trip, user_id: 1)}
         let!(:trip2) { create(:trip, user_id: 1)}
         let(:user_id) { trip1.user_id}
+
         run_test!
       end
     end
@@ -34,7 +35,7 @@ RSpec.describe 'Trips API', type: :request do
       description "Creates a Trip for a User with all Trip information"
       parameter name: :trip, in: :body, schema: { "$ref" => "#/components/schemas/Trip" }
 
-      response(201, 'trip created') do
+      response(201, 'Trip created') do
         let!(:trip) {
           {
             name: "Visiting Family",
@@ -45,6 +46,21 @@ RSpec.describe 'Trips API', type: :request do
             user_id: 1
           }
         }
+
+        run_test!
+      end
+
+      response(400, "Validation failed") do
+        let(:trip) {
+          {
+            location: "Brazil",
+            start_date: DateTime.new(2024,12,10),
+            end_date: DateTime.new(2025,1,10),
+            total_budget: 10000,
+            user_id: 1
+          }
+        }
+
         run_test!
       end
     end
@@ -60,10 +76,28 @@ RSpec.describe 'Trips API', type: :request do
       produces 'application/json'
       description "List a Trip that belongs to a user with all Trip information and Daily Itineraries"
 
-      response(200, 'successful') do
+      response(200, 'Successful') do
         let!(:trip1) { create(:trip, user_id: 1)}
         let(:user_id) { trip1.user_id}
-        let(:id) { trip.id }
+        let(:id) { trip1.id }
+
+        run_test!
+      end
+
+      response(404, "Couldn't find Trip with 'id'=2") do
+        let!(:trip1) { create(:trip, user_id: 1)}
+        let(:user_id) { trip1.user_id}
+        let(:id) { 2 }
+
+        run_test!
+      end
+
+      response(400, "Validation failed: Invalid User ID provided") do
+        let!(:trip1) { create(:trip, user_id: 1)}
+        let(:user_id) { 2 }
+        let(:id) { trip1.id }
+
+        run_test!
       end
     end
 
@@ -74,10 +108,30 @@ RSpec.describe 'Trips API', type: :request do
       description "Updates a User's Trip information"
       parameter name: :trip, in: :body, schema: { "$ref" => "#/components/schemas/Trip" }
 
-      response(200, 'trip updated') do
-        let!(:trip) { create(:trip, user_id: 1)}
-        let(:id) { trip.id }
-        let(:user_id) {trip.user_id}
+      response(200, 'Trip updated') do
+        let!(:trip1) { create(:trip, user_id: 1)}
+        let(:id) { trip1.id }
+        let(:user_id) {trip1.user_id}
+
+        let(:trip) {{name: "Disneyland"}}
+
+        run_test!
+      end
+
+      response(400, "Validation failed: Name can't be blank") do
+        let!(:trip1) { create(:trip, user_id: 1)}
+        let(:id) { trip1.id }
+        let(:user_id) {trip1.user_id}
+        let(:trip) {{name: nil}}
+
+        run_test!
+      end
+
+      response(404, "Couldn't find Trip with 'id'=12") do
+        let!(:trip1) { create(:trip, user_id: 1)}
+        let(:id) { 12 }
+        let(:user_id) {trip1.user_id}
+        let(:trip) {{name: "Paris!"}}
 
         run_test!
       end
@@ -92,6 +146,14 @@ RSpec.describe 'Trips API', type: :request do
       response(204, 'trip deleted') do
         let!(:trip) { create(:trip, user_id: 1)}
         let(:id) { trip.id }
+        let(:user_id) {trip.user_id}
+
+        run_test!
+      end
+
+      response(404, "Couldn't find Trip with 'id'=12") do
+        let!(:trip) { create(:trip, user_id: 1)}
+        let(:id) { 12 }
         let(:user_id) {trip.user_id}
 
         run_test!
