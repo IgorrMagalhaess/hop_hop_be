ee4e7bf5b474f1364534fee2575e7352a13fb2a9
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-04-12 19:00:22 -0700

    fix: Revert unintended changes

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :trip do
     name { Faker::Name.name }
     location { Faker::Address.country }
-    start_date { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now + 1) }
+    start_date { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
     end_date { Faker::Time.between(from: DateTime.now + 1, to: DateTime.now + 3) }
     status { Faker::Number.between(from: 0, to: 1) }
     total_budget { Faker::Number.between(from: 500, to: 10000) }
