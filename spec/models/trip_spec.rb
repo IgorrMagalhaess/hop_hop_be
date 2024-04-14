31f2cddd0c162e0967bb7e33e589c1676114a5ff
Hoa Dam <hoangocdam@gmail.com>
2024-04-14 15:22:13 -0700

    feat:test create and update daily_itineraries

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Trip, type: :model do
          expect(trip.errors[:end_date]).to include("must be greater than #{trip.start_date}")
       end
    end
-   
+
    describe 'relationships' do
       it { should have_many :daily_itineraries }
       it { should have_many(:activities).through(:daily_itineraries) }
@@ -39,5 +39,122 @@ RSpec.describe Trip, type: :model do
          expect(Trip.trips_by_user_id(1).first).to_not eq(trips_2.first)
       end
    end
-end
 
+   describe "create or delete daily_itineraries when edit start_date of the trip" do
+      it "deletes daily_itineraries when user changes the start_date to later" do
+         trip = Trip.create!({
+            name: "Trip To Japan",
+            location: "Japan",
+            start_date: DateTime.new(2024,5,1),
+            end_date: DateTime.new(2024,5,10),
+            total_budget: 10000,
+            user_id: 2
+         })
+
+         trip.update!({
+            start_date: DateTime.new(2024,5,3)
+         })
+
+         expected_days = (DateTime.new(2024,5,3).to_date..DateTime.new(2024,5,10).to_date).count
+
+         expect(trip.daily_itineraries.count).to eq(expected_days)
+      end
+
+      it "creates daily_itineraries when user changes the start_date to earlier" do
+         trip = Trip.create!({
+            name: "Trip To Japan",
+            location: "Japan",
+            start_date: DateTime.new(2024,5,1),
+            end_date: DateTime.new(2024,5,10),
+            total_budget: 10000,
+            user_id: 2
+         })
+
+         trip.update!({
+            start_date: DateTime.new(2024,4,28)
+         })
+
+         expected_days = (DateTime.new(2024,4,28).to_date..DateTime.new(2024,5,10).to_date).count
+
+         expect(trip.daily_itineraries.count).to eq(expected_days)
+      end
+   end
+
+   describe "create or delete daily_itineraries when edit end_date of the trip" do
+      it "deletes daily_itineraries when user changes the end_date to earlier" do
+         trip = Trip.create!({
+            name: "Trip To Japan",
+            location: "Japan",
+            start_date: DateTime.new(2024,5,1),
+            end_date: DateTime.new(2024,5,10),
+            total_budget: 10000,
+            user_id: 2
+         })
+
+         trip.update!({
+            end_date: DateTime.new(2024,5,8)
+         })
+
+         expected_days = (DateTime.new(2024,5,1).to_date..DateTime.new(2024,5,8).to_date).count
+         expect(trip.daily_itineraries.count).to eq(expected_days)
+      end
+
+      it "creates daily_itineraries when user changes the end_date to later" do
+         trip = Trip.create!({
+            name: "Trip To Japan",
+            location: "Japan",
+            start_date: DateTime.new(2024,5,1),
+            end_date: DateTime.new(2024,5,12),
+            total_budget: 10000,
+            user_id: 2
+         })
+
+         trip.update!({
+            end_date: DateTime.new(2024,5,12)
+         })
+
+         expected_days = (DateTime.new(2024,5,1).to_date..DateTime.new(2024,5,12).to_date).count
+         expect(trip.daily_itineraries.count).to eq(expected_days)
+      end
+
+      describe "create or delete daily_itineraries when edit both start_date and end_date of the trip" do
+         it "creates daily_itineraries when user changes the start_date to earlier and deletes daily_itineraries when user changes the end_date to earlier" do
+            trip = Trip.create!({
+            name: "Trip To Japan",
+            location: "Japan",
+            start_date: DateTime.new(2024,5,1),
+            end_date: DateTime.new(2024,5,12),
+            total_budget: 10000,
+            user_id: 2
+            })
+
+            trip.update!({
+               start_date: DateTime.new(2024,4,28),
+               end_date: DateTime.new(2024,5,10)
+            })
+
+            expected_days = (DateTime.new(2024,4,28).to_date..DateTime.new(2024,5,10).to_date).count
+            expect(trip.daily_itineraries.count).to eq(expected_days)
+         end
+
+         it "deletes daily_itineraries when user changes the start_date to later and creates daily_itineraries when user changes the end_date to later" do
+            trip = Trip.create!({
+            name: "Trip To Japan",
+            location: "Japan",
+            start_date: DateTime.new(2024,5,1),
+            end_date: DateTime.new(2024,5,12),
+            total_budget: 10000,
+            user_id: 2
+            })
+
+            trip.update!({
+               start_date: DateTime.new(2024,5,3),
+               end_date: DateTime.new(2024,5,15)
+            })
+
+            expected_days = (DateTime.new(2024,5,3).to_date..DateTime.new(2024,5,15).to_date).count
+            expect(trip.daily_itineraries.count).to eq(expected_days)
+         end
+      end
+   end
+end
