dc188335f3a5bd8021267cf7ad658a0566087fe8
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-04-14 16:48:58 -0600

    Feat: add trip decorator

---
+++ app/decorators/trip_decorator.rb
@@ -0,0 +1,20 @@
+class TripDecorator < Draper::Decorator
+  delegate_all
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+  def start_date_format
+    object.start_date.strftime("%B %d, %Y")
+  end
+
+  def end_date_format
+    object.end_date.strftime("%B %d, %Y")
+  end
+end
